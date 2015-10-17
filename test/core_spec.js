f2f81429ea4c9377862f64a9085229c113ff3de8
thtruo <ttruongchau@gmail.com>
2015-10-17 12:53:21 -0700

    included test and implementation for `next` to start the vote

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -10,6 +10,20 @@ describe('application logic', () => {
       const nextState = setEntries(state, entries);
       expect(nextState).to.equal(Map({
         entries: List.of('Batman Begins', 'The Dark Knight')
+      }));
+    });
+  });
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Batman Begins', 'The Dark Knight','The Dark Knight Rises', 'Batman v Superman')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Batman Begins', 'The Dark Knight')
+        }),
+        entries: List.of('The Dark Knight Rises', 'Batman v Superman')
       }));
     });
   });
