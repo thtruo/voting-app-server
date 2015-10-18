e876e935392319764fd4817fb88453c066a2a25f
thtruo <ttruongchau@gmail.com>
2015-10-17 17:45:53 -0700

    added implementation and test for reducer and actions (this model is for Redux)

---
+++ test/reducer_spec.js
@@ -0,0 +1,71 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+
+  it('handles SET_ENTRIES', () => {
+    const initialState = Map();
+    const action = {type: 'SET_ENTRIES', entries: ['Batman Begins']};
+    const nextState = reducer(initialState, action);
+    expect(nextState).to.equal(fromJS({
+      entries: ['Batman Begins']
+    }));
+  });
+
+  it('handles NEXT', () => {
+    const initialState = fromJS({
+      entries: ['Batman Begins', 'The Dark Knight']
+    });
+    const action = {type: 'NEXT'};
+    const nextState = reducer(initialState, action);
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Batman Begins', 'The Dark Knight']
+      },
+      entries: []
+    }));
+  });
+
+  it('handles VOTE', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Batman Begins', 'The Dark Knight']
+      },
+      entries: []
+    });
+    const action = {type: 'VOTE', entry: 'The Dark Knight'};
+    const nextState = reducer(initialState, action);
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Batman Begins', 'The Dark Knight'],
+        tally: {'The Dark Knight': 1}
+      },
+      entries: []
+    }));
+  });
+
+  it('has an initial state', () => {
+    const action = {type: 'SET_ENTRIES', entries: ['Batman v Superman']};
+    const nextState = reducer(undefined, action);
+    expect(nextState).to.equal(fromJS({
+      entries: ['Batman v Superman']
+    }));
+  });
+
+  it('can be used with reduce', () => {
+    const actions = [
+      {type: 'SET_ENTRIES', entries: ['Batman Begins', 'The Dark Knight']},
+      {type: 'NEXT'},
+      {type: 'VOTE', 'The Dark Knight'},
+      {type: 'VOTE', 'Batman Begins'},
+      {type: 'VOTE', 'The Dark Knight'},
+      {type: 'NEXT'}
+    ];
+    const finalState = actions.reduce(reducer, Map());
+    expect(finalState).to.equal(fromJS({
+      winner: 'The Dark Knight'
+    }));
+  });
+
+});
