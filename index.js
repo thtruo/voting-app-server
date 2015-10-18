e04fe88d09617d67ccd341441e4a09985bfcb697
thtruo <ttruongchau@gmail.com>
2015-10-17 19:57:30 -0700

    play with test entries

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ import startServer from './src/server';
 
 export const store = makeStore();
 startServer(store);
+
+// Load up set of test entries and kick off vote by dispatching NEXT
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({
+  type: 'NEXT'
+});
