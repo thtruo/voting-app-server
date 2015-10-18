ca2e88feb2906e30b8051ecde38d6b5eb7aab6ea
thtruo <ttruongchau@gmail.com>
2015-10-17 19:46:28 -0700

    integrate server and store by broadcasting state and receiving remote actions

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import makeStore from './src/store';
 import startServer from './src/server';
 
 export const store = makeStore();
-startServer();
+startServer(store);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  // Send a state snapshopt whenever state (potentially) changes
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  // Clients immediately receives/updates current state upon connection
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
 }
