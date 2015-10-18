bf3e640638713404c6c067a4a2b49bed6b515032
thtruo <ttruongchau@gmail.com>
2015-10-17 19:16:30 -0700

    set up `Socket.io` server and add `start` command to `package.json`

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* Entry point for the application creates and exports a store
  */
 import makeStore from './src/store';
+import startServer from './src/server';
 
 export const store = makeStore();
+startServer();
